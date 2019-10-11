93827dfe96edaa0f92a47c944eeb46e046bbac5a
Blake Warner <blake@null3d.com>
2019-10-11 00:57:24 -0400

    skip delta fragments when compressing NSZ

--- compressor/nut/__init__.py
+++ compressor/nut/__init__.py
@@ -59,6 +59,10 @@ def compress(filePath, compressionLevel = 17, outputDir = None):
 	newNsp = Fs.Pfs0.Pfs0Stream(nszPath)
 
 	for nspf in container:
+		if isinstance(nspf, Fs.Nca.Nca) and nspf.header.contentType == Fs.Type.Content.DATA:
+			Print.info('skipping delta fragment')
+			continue
+			
 		if isinstance(nspf, Fs.Nca.Nca) and (nspf.header.contentType == Fs.Type.Content.PROGRAM or nspf.header.contentType == Fs.Type.Content.PUBLICDATA):
 			if isNcaPacked(nspf):
 				cctx = zstandard.ZstdCompressor(level=compressionLevel)
