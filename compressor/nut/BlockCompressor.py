1b5d91686ccddffdb186910649e7fe0151f07e76
plato79 <h_atac@yahoo.com>
2019-10-17 14:38:24 +0300

    Delete file if exception occurs..

--- compressor/nut/BlockCompressor.py
+++ compressor/nut/BlockCompressor.py
@@ -100,119 +100,126 @@ def blockCompress(filePath, compressionLevel = 17, blockSizeExponent = 19, outpu
 	
 	newNsp = Fs.Pfs0.Pfs0Stream(nszPath)
 
-	for nspf in container:
-		if isinstance(nspf, Fs.Nca.Nca) and nspf.header.contentType == Fs.Type.Content.DATA:
-			Print.info('skipping delta fragment')
-			continue
-			
-		if isinstance(nspf, Fs.Nca.Nca) and (nspf.header.contentType == Fs.Type.Content.PROGRAM or nspf.header.contentType == Fs.Type.Content.PUBLICDATA):
-			if SectionFs.isNcaPacked(nspf, ncaHeaderSize):
-				
-				newFileName = nspf._path[0:-1] + 'z'
-				
-				f = newNsp.add(newFileName, nspf.size)
-				
-				start = f.tell()
-				
-				nspf.seek(0)
-				f.write(nspf.read(ncaHeaderSize))
-				
-				sections = []
-				for fs in SectionFs.sortedFs(nspf):
-					sections += fs.getEncryptionSections()
-				
-				header = b'NCZSECTN'
-				header += len(sections).to_bytes(8, 'little')
-				
-				i = 0
-				for fs in sections:
-					i += 1
-					header += fs.offset.to_bytes(8, 'little')
-					header += fs.size.to_bytes(8, 'little')
-					header += fs.cryptoType.to_bytes(8, 'little')
-					header += b'\x00' * 8
-					header += fs.cryptoKey
-					header += fs.cryptoCounter
-					
-				f.write(header)
-				
-				blockID = 0
-				chunkRelativeBlockID = 0
-				startChunkBlockID = 0
-				blocksHeaderFilePos = f.tell()
-				compressedblockSizeList = []
-				
-				bytesToCompress = nspf.size-0x4000
-				blocksToCompress = bytesToCompress//blockSize + (bytesToCompress%blockSize > 0)
-				header = b'NCZBLOCK' #Magic
-				header += b'\x02' #Version
-				header += b'\x01' #Type
-				header += b'\x00' #Unused
-				header += blockSizeExponent.to_bytes(1, 'little') #blockSizeExponent in bits: 2^x
-				header += blocksToCompress.to_bytes(4, 'little') #Amount of Blocks
-				header += bytesToCompress.to_bytes(8, 'little') #Decompressed Size
-				header += b'\x00' * (blocksToCompress*4)
-				f.write(header)
-				compressedblockSizeList = [0]*blocksToCompress
-				
-				decompressedBytes = ncaHeaderSize
-				
-				with tqdm(total=nspf.size, unit_scale=True, unit="B/s") as bar:
-					
-					partitions = []
-					for section in sections:
-						#print('offset: %x\t\tsize: %x\t\ttype: %d\t\tiv%s' % (section.offset, section.size, section.cryptoType, str(hx(section.cryptoCounter))))
-						partitions.append(nspf.partition(offset = section.offset, size = section.size, n = None, cryptoType = section.cryptoType, cryptoKey = section.cryptoKey, cryptoCounter = bytearray(section.cryptoCounter), autoOpen = True))
-						
-					
-					partNr = 0
-					bar.update(f.tell())
-					while True:
-						buffer = partitions[partNr].read(blockSize)
-						while (len(buffer) < blockSize and partNr < len(partitions)-1):
-							partNr += 1
-							buffer += partitions[partNr].read(blockSize - len(buffer))
-						
-						if chunkRelativeBlockID >= TasksPerChunk or len(buffer) == 0:
-							while readyForWork.value() < threads:
-								sleep(0.02)
-							for i in range(min(TasksPerChunk, blocksToCompress-startChunkBlockID)):
-								compressedblockSizeList[startChunkBlockID+i] = len(results[i])
-								f.write(results[i])
-								results[i] = b""
-							if len(buffer) == 0:
-								pleaseKillYourself.increment()
-								for i in range(readyForWork.value()):
-									work.put(None)
-								while readyForWork.value() > 0:
-									sleep(0.02)
-								break
-							chunkRelativeBlockID = 0
-							startChunkBlockID = blockID
-						work.put([buffer, compressionLevel, compressedblockSizeList, chunkRelativeBlockID])
-						blockID += 1
-						chunkRelativeBlockID += 1
-						decompressedBytes += len(buffer)
-						bar.update(len(buffer))
-				
-				f.seek(blocksHeaderFilePos+24)
-				header = b""
-				for compressedblockSize in compressedblockSizeList:
-					header += compressedblockSize.to_bytes(4, 'little')
-				f.write(header)
-				f.seek(0, 2) #Seek to end of file.
-				
-				written = f.tell() - start
-				print('compressed %d%% %d -> %d  - %s' % (int(written * 100 / nspf.size), decompressedBytes, written, nspf._path))
-				newNsp.resize(newFileName, written)
-				continue
-			else:
-				print('not packed!')
-
-		f = newNsp.add(nspf._path, nspf.size)
-		nspf.seek(0)
-		while not nspf.eof():
-			buffer = nspf.read(CHUNK_SZ)
-			f.write(buffer)
+	try:
 	
-	newNsp.close()
+		for nspf in container:
+			if isinstance(nspf, Fs.Nca.Nca) and nspf.header.contentType == Fs.Type.Content.DATA:
+				Print.info('skipping delta fragment')
+				continue
+				
+			if isinstance(nspf, Fs.Nca.Nca) and (nspf.header.contentType == Fs.Type.Content.PROGRAM or nspf.header.contentType == Fs.Type.Content.PUBLICDATA):
+				if SectionFs.isNcaPacked(nspf, ncaHeaderSize):
+					
+					newFileName = nspf._path[0:-1] + 'z'
+					
+					f = newNsp.add(newFileName, nspf.size)
+					
+					start = f.tell()
+					
+					nspf.seek(0)
+					f.write(nspf.read(ncaHeaderSize))
+					
+					sections = []
+					for fs in SectionFs.sortedFs(nspf):
+						sections += fs.getEncryptionSections()
+					
+					header = b'NCZSECTN'
+					header += len(sections).to_bytes(8, 'little')
+					
+					i = 0
+					for fs in sections:
+						i += 1
+						header += fs.offset.to_bytes(8, 'little')
+						header += fs.size.to_bytes(8, 'little')
+						header += fs.cryptoType.to_bytes(8, 'little')
+						header += b'\x00' * 8
+						header += fs.cryptoKey
+						header += fs.cryptoCounter
+						
+					f.write(header)
+					
+					blockID = 0
+					chunkRelativeBlockID = 0
+					startChunkBlockID = 0
+					blocksHeaderFilePos = f.tell()
+					compressedblockSizeList = []
+					
+					bytesToCompress = nspf.size-0x4000
+					blocksToCompress = bytesToCompress//blockSize + (bytesToCompress%blockSize > 0)
+					header = b'NCZBLOCK' #Magic
+					header += b'\x02' #Version
+					header += b'\x01' #Type
+					header += b'\x00' #Unused
+					header += blockSizeExponent.to_bytes(1, 'little') #blockSizeExponent in bits: 2^x
+					header += blocksToCompress.to_bytes(4, 'little') #Amount of Blocks
+					header += bytesToCompress.to_bytes(8, 'little') #Decompressed Size
+					header += b'\x00' * (blocksToCompress*4)
+					f.write(header)
+					compressedblockSizeList = [0]*blocksToCompress
+					
+					decompressedBytes = ncaHeaderSize
+					
+					with tqdm(total=nspf.size, unit_scale=True, unit="B/s") as bar:
+						
+						partitions = []
+						for section in sections:
+							#print('offset: %x\t\tsize: %x\t\ttype: %d\t\tiv%s' % (section.offset, section.size, section.cryptoType, str(hx(section.cryptoCounter))))
+							partitions.append(nspf.partition(offset = section.offset, size = section.size, n = None, cryptoType = section.cryptoType, cryptoKey = section.cryptoKey, cryptoCounter = bytearray(section.cryptoCounter), autoOpen = True))
+							
+						
+						partNr = 0
+						bar.update(f.tell())
+						while True:
+							buffer = partitions[partNr].read(blockSize)
+							while (len(buffer) < blockSize and partNr < len(partitions)-1):
+								partNr += 1
+								buffer += partitions[partNr].read(blockSize - len(buffer))
+							
+							if chunkRelativeBlockID >= TasksPerChunk or len(buffer) == 0:
+								while readyForWork.value() < threads:
+									sleep(0.02)
+								for i in range(min(TasksPerChunk, blocksToCompress-startChunkBlockID)):
+									compressedblockSizeList[startChunkBlockID+i] = len(results[i])
+									f.write(results[i])
+									results[i] = b""
+								if len(buffer) == 0:
+									pleaseKillYourself.increment()
+									for i in range(readyForWork.value()):
+										work.put(None)
+									while readyForWork.value() > 0:
+										sleep(0.02)
+									break
+								chunkRelativeBlockID = 0
+								startChunkBlockID = blockID
+							work.put([buffer, compressionLevel, compressedblockSizeList, chunkRelativeBlockID])
+							blockID += 1
+							chunkRelativeBlockID += 1
+							decompressedBytes += len(buffer)
+							bar.update(len(buffer))
+					
+					f.seek(blocksHeaderFilePos+24)
+					header = b""
+					for compressedblockSize in compressedblockSizeList:
+						header += compressedblockSize.to_bytes(4, 'little')
+					f.write(header)
+					f.seek(0, 2) #Seek to end of file.
+					
+					written = f.tell() - start
+					print('compressed %d%% %d -> %d  - %s' % (int(written * 100 / nspf.size), decompressedBytes, written, nspf._path))
+					newNsp.resize(newFileName, written)
+					continue
+				else:
+					print('not packed!')
+
+			f = newNsp.add(nspf._path, nspf.size)
+			nspf.seek(0)
+			while not nspf.eof():
+				buffer = nspf.read(CHUNK_SZ)
+				f.write(buffer)
+		
+		newNsp.close()
+
+	except BaseException as e:
+		Print.error(str(e))
+		newNsp.close()
+		os.remove(nszPath)
--- compressor/nut/SolidCompressor.py
+++ compressor/nut/SolidCompressor.py
@@ -57,90 +57,96 @@ def solidCompress(filePath, compressionLevel = 17, outputDir = None, threads = -
 	
 	newNsp = Fs.Pfs0.Pfs0Stream(nszPath)
 	
-	for nspf in container:
+	try:
 
-		if isinstance(nspf, Fs.Nca.Nca) and nspf.header.contentType == Fs.Type.Content.DATA:
-			Print.info('skipping delta fragment')
-			continue
-			
-		if isinstance(nspf, Fs.Nca.Nca) and (nspf.header.contentType == Fs.Type.Content.PROGRAM or nspf.header.contentType == Fs.Type.Content.PUBLICDATA):
-			if SectionFs.isNcaPacked(nspf, ncaHeaderSize):
-				
-				newFileName = nspf._path[0:-1] + 'z'
-				
-				f = newNsp.add(newFileName, nspf.size)
-				
-				start = f.tell()
-				
-				nspf.seek(0)
-				f.write(nspf.read(ncaHeaderSize))
-				
-				sections = []
-				for fs in SectionFs.sortedFs(nspf):
-					sections += fs.getEncryptionSections()
-				
-				header = b'NCZSECTN'
-				header += len(sections).to_bytes(8, 'little')
-				
-				i = 0
-				for fs in sections:
-					i += 1
-					header += fs.offset.to_bytes(8, 'little')
-					header += fs.size.to_bytes(8, 'little')
-					header += fs.cryptoType.to_bytes(8, 'little')
-					header += b'\x00' * 8
-					header += fs.cryptoKey
-					header += fs.cryptoCounter
-					
-				f.write(header)
-				
-				blockID = 0
-				chunkRelativeBlockID = 0
-				startChunkBlockID = 0
-				blocksHeaderFilePos = f.tell()
-				compressedblockSizeList = []
-				
-				decompressedBytes = ncaHeaderSize
-				
-				with tqdm(total=nspf.size, unit_scale=True, unit="B/s") as bar:
-					
-					partitions = []
-					for section in sections:
-						#print('offset: %x\t\tsize: %x\t\ttype: %d\t\tiv%s' % (section.offset, section.size, section.cryptoType, str(hx(section.cryptoCounter))))
-						partitions.append(nspf.partition(offset = section.offset, size = section.size, n = None, cryptoType = section.cryptoType, cryptoKey = section.cryptoKey, cryptoCounter = bytearray(section.cryptoCounter), autoOpen = True))
-						
-					
-					partNr = 0
-					bar.update(f.tell())
-					cctx = zstandard.ZstdCompressor(level=compressionLevel)
-					compressor = cctx.stream_writer(f)
-					while True:
-					
-						buffer = partitions[partNr].read(CHUNK_SZ)
-						while (len(buffer) < CHUNK_SZ and partNr < len(partitions)-1):
-							partNr += 1
-							buffer += partitions[partNr].read(CHUNK_SZ - len(buffer))
-						if len(buffer) == 0:
-							break
-						compressor.write(buffer)
-						
-						decompressedBytes += len(buffer)
-						bar.update(len(buffer))
-				
-				compressor.flush(zstandard.FLUSH_FRAME)
-				compressor.flush(zstandard.COMPRESSOBJ_FLUSH_FINISH)
-				
-				written = f.tell() - start
-				print('compressed %d%% %d -> %d  - %s' % (int(written * 100 / nspf.size), decompressedBytes, written, nspf._path))
-				newNsp.resize(newFileName, written)
+		for nspf in container:
+
+			if isinstance(nspf, Fs.Nca.Nca) and nspf.header.contentType == Fs.Type.Content.DATA:
+				Print.info('skipping delta fragment')
 				continue
-			else:
-				print('not packed!')
+				
+			if isinstance(nspf, Fs.Nca.Nca) and (nspf.header.contentType == Fs.Type.Content.PROGRAM or nspf.header.contentType == Fs.Type.Content.PUBLICDATA):
+				if SectionFs.isNcaPacked(nspf, ncaHeaderSize):
+					
+					newFileName = nspf._path[0:-1] + 'z'
+					
+					f = newNsp.add(newFileName, nspf.size)
+					
+					start = f.tell()
+					
+					nspf.seek(0)
+					f.write(nspf.read(ncaHeaderSize))
+					
+					sections = []
+					for fs in SectionFs.sortedFs(nspf):
+						sections += fs.getEncryptionSections()
+					
+					header = b'NCZSECTN'
+					header += len(sections).to_bytes(8, 'little')
+					
+					i = 0
+					for fs in sections:
+						i += 1
+						header += fs.offset.to_bytes(8, 'little')
+						header += fs.size.to_bytes(8, 'little')
+						header += fs.cryptoType.to_bytes(8, 'little')
+						header += b'\x00' * 8
+						header += fs.cryptoKey
+						header += fs.cryptoCounter
+						
+					f.write(header)
+					
+					blockID = 0
+					chunkRelativeBlockID = 0
+					startChunkBlockID = 0
+					blocksHeaderFilePos = f.tell()
+					compressedblockSizeList = []
+					
+					decompressedBytes = ncaHeaderSize
+					
+					with tqdm(total=nspf.size, unit_scale=True, unit="B/s") as bar:
+						
+						partitions = []
+						for section in sections:
+							#print('offset: %x\t\tsize: %x\t\ttype: %d\t\tiv%s' % (section.offset, section.size, section.cryptoType, str(hx(section.cryptoCounter))))
+							partitions.append(nspf.partition(offset = section.offset, size = section.size, n = None, cryptoType = section.cryptoType, cryptoKey = section.cryptoKey, cryptoCounter = bytearray(section.cryptoCounter), autoOpen = True))
+							
+						
+						partNr = 0
+						bar.update(f.tell())
+						cctx = zstandard.ZstdCompressor(level=compressionLevel)
+						compressor = cctx.stream_writer(f)
+						while True:
+						
+							buffer = partitions[partNr].read(CHUNK_SZ)
+							while (len(buffer) < CHUNK_SZ and partNr < len(partitions)-1):
+								partNr += 1
+								buffer += partitions[partNr].read(CHUNK_SZ - len(buffer))
+							if len(buffer) == 0:
+								break
+							compressor.write(buffer)
+							
+							decompressedBytes += len(buffer)
+							bar.update(len(buffer))
+					
+					compressor.flush(zstandard.FLUSH_FRAME)
+					compressor.flush(zstandard.COMPRESSOBJ_FLUSH_FINISH)
+					
+					written = f.tell() - start
+					print('compressed %d%% %d -> %d  - %s' % (int(written * 100 / nspf.size), decompressedBytes, written, nspf._path))
+					newNsp.resize(newFileName, written)
+					continue
+				else:
+					print('not packed!')
 
-		f = newNsp.add(nspf._path, nspf.size)
-		nspf.seek(0)
-		while not nspf.eof():
-			buffer = nspf.read(CHUNK_SZ)
-			f.write(buffer)
-	
-	newNsp.close()
+			f = newNsp.add(nspf._path, nspf.size)
+			nspf.seek(0)
+			while not nspf.eof():
+				buffer = nspf.read(CHUNK_SZ)
+				f.write(buffer)
+		newNsp.close()
+
+	except BaseException as e:
+		Print.error(str(e))
+		newNsp.close()
+		os.remove(nszPath)
