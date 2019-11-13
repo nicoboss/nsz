5aeea2c8ac868181bc7f9415e84854c5d2cbe72c
nicoboss <nico@bosshome.ch>
2019-11-13 02:00:36 +0100

    Moved nsz.py to nsz\__init__.py

---
+++ nsz/__init__old.py
@@ -0,0 +1,22 @@
+import multiprocessing
+from nsz import BlockCompressor
+from nsz import SolidCompressor
+from nsz import NszDecompressor
+
+def compress(filePath, args):
+	compressionLevel = 18 if args.level is None else args.level
+	if args.threads < 1:
+		threads = multiprocessing.cpu_count()
+	if args.block:
+		outFile = BlockCompressor.blockCompress(filePath, compressionLevel, args.bs)
+	else:
+		outFile = SolidCompressor.solidCompress(filePath, compressionLevel, args.output, args.threads, args.overwrite)
+	if args.verify:
+		print("[VERIFY NSZ] {0}".format(outFile))
+		verify(outFile, True)
+
+def decompress(filePath, outputDir = None):
+	NszDecompressor.decompress(filePath, outputDir)
+
+def verify(filePath, raiseVerificationException):
+	NszDecompressor.verify(filePath, raiseVerificationException)
