50454c4590329dee905153e7cba2fdbd6b6746a5
nicoboss <nico@bosshome.ch>
2019-11-14 11:00:52 +0100

    Implemented python version checking to prevent python from showing the users confusing compatibility related exceptions

--- nsz.py
+++ nsz.py
@@ -4,6 +4,9 @@
 # This if is needed as multiprocessing shouldn't include nsz
 # as it won't be able to optain __main__.__file__ and so crash inside Keys.py
 if __name__ == '__main__':
+	import sys
+	if sys.hexversion < 0x03060000:
+		raise ImportError("NSZ requires at least Python 3.6!\nCurrent python version is " + sys.version)
 	import nsz
 	import multiprocessing
 	multiprocessing.freeze_support()
