e8b3540af9218459a6248109094bc9ce0c74ffdf
nicoboss <nico@bosshome.ch>
2019-12-29 16:20:05 +0100

    Updated testing and deployment scripts again

--- dev/test.cmd
+++ dev/test.cmd
@@ -13,22 +13,30 @@ goto :eof
 
 :Test1
 cd ROMs
-del *.nsz
-python ..\nsz.py -l 8 -V -C .
-del *.nsz
+python ..\nsz.py -V .
+del *.nsz *.xcz
+python ..\nsz.py -l 8 -V -S -C .
+del *.nsz *.xcz
 python ..\nsz.py -l 8 -V -B -C .
+del *.nsp *.xci
+nsz -D -V .
 cd ..
 goto :eof
 
 :Test2
+rmdir /s /q build
+rmdir /s /q dist
 python setup.py sdist bdist_wheel
 pip uninstall nsz -y
 pip install .
 cd ROMs
-del *.nsz
-nsz -l 8 -V -C .
-del *.nsz
+nsz -V .
+del *.nsz *.xcz
+nsz -l 8 -V -S -C .
+del *.nsz *.xcz
 nsz -l 8 -V -B -C .
+del *.nsp *.xci
+nsz -D -V .
 cd ..
 goto :eof
 
@@ -41,9 +49,12 @@ pyinstaller .\__init__.spec
 mkdir dist\__init__\gui
 cd ..
 cd ROMs
-del *.nsz
-..\nsz\dist\__init__\nsz.exe -l 8 -V -C .
-del *.nsz
+..\nsz\dist\__init__\nsz.exe -V .
+del *.nsz *.xcz
+..\nsz\dist\__init__\nsz.exe -l 8 -S -V -C .
+del *.nsz *.xcz
 ..\nsz\dist\__init__\nsz.exe -l 8 -V -B -C .
+del *.nsp *.xci
+..\nsz\dist\__init__\nsz.exe -D -V .
 cd ..
 goto :eof
--- dev/testPip.cmd
+++ dev/testPip.cmd
@@ -1,4 +1,7 @@
+@echo OFF
 cd ..
+rmdir /s /q build
+rmdir /s /q dist
 python setup.py sdist bdist_wheel
 pip uninstall nsz -y
 pip install .
