652ea4a04d48787a6cfd644a47e0b0d39a6d24a5
nicoboss <nico@bosshome.ch>
2019-12-29 09:49:04 +0100

    Updated testing and deployment scripts

--- dev/test.cmd
+++ dev/test.cmd
@@ -4,23 +4,19 @@ REM Tests
 cd ..
 call :Test1
 pause
-cls
 call :Test2
 pause
-cls
 call :Test3
 pause
-cls
 goto :eof
 
 
 :Test1
 cd ROMs
 del *.nsz
-python ..\nsz.py -C -V . -l 0
+python ..\nsz.py -l 8 -V -C .
 del *.nsz
-python ..\nsz.py -B -C -V .
-del *.nsz
+python ..\nsz.py -l 8 -V -B -C .
 cd ..
 goto :eof
 
@@ -30,27 +26,24 @@ pip uninstall nsz -y
 pip install .
 cd ROMs
 del *.nsz
-nsz -C -V . -l 0
+nsz -l 8 -V -C .
 del *.nsz
-nsz -B -C -V .
-del *.nsz
+nsz -l 8 -V -B -C .
 cd ..
 goto :eof
 
 :Test3
 cd nsz
-rmdir /s /q __init__.dist
-call nuitka __init__.py --standalone --plugin-enable=multiprocessing
-move __init__.dist\__init__.exe __init__.dist\nsz.exe
-rename __init__.dist nsz_win64_portable
-rmdir /s /q ..\nsz_win64_portable
-move nsz_win64_portable ..
+rmdir /s /q build
+rmdir /s /q dist
+ping 127.0.0.1 -n 2 >NUL
+pyinstaller .\__init__.spec
+mkdir dist\__init__\gui
 cd ..
 cd ROMs
 del *.nsz
-..\nsz_win64_portable\nsz.exe -C -V . -l 0
+..\nsz\dist\__init__\nsz.exe -l 8 -V -C .
 del *.nsz
-..\nsz_win64_portable\nsz.exe -B -C -V .
-del *.nsz
+..\nsz\dist\__init__\nsz.exe -l 8 -V -B -C .
 cd ..
 goto :eof
--- dev/testPyInstaller.cmd
+++ dev/testPyInstaller.cmd
@@ -1,4 +1,8 @@
+@echo OFF
 cd ..\nsz
+rmdir /s /q build
+rmdir /s /q dist
+ping 127.0.0.1 -n 2 >NUL
 pyinstaller .\__init__.spec
 cd dist\__init__
 mkdir gui
