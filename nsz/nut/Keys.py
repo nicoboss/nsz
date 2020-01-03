db10e4eca2a3e3ad7ff47b56616b4f812792e035
nicoboss <nico@bosshome.ch>
2020-01-03 20:05:51 +0100

    Implemented CRC32 key validation, added support for future masterkeys up to masterkey_31, made the key file path inside the missing key error message pointing to the currently loaded key file, switched Keys.py to pathlib and new formatting syntax and no longer throwing errors for missing unneeded keys as requested in #45 and #51

--- nsz/nut/Keys.py
+++ nsz/nut/Keys.py
@@ -1,13 +1,41 @@
 import os, sys, re
 from traceback import format_exc
 from nut import aes128
-from binascii import hexlify as hx, unhexlify as uhx
+from binascii import crc32, hexlify as hx, unhexlify as uhx
 from nut import Print
 from pathlib import Path
+from multiprocessing import *
 
 keys = {}
 titleKeks = []
 keyAreaKeys = []
+loadedKeysFile = "non-existing prod.keys/keys.txt"
+
+#This are NOT the keys but only a 4 bytes long checksum!
+#See https://en.wikipedia.org/wiki/Cyclic_redundancy_check
+#An infinite amount of inputs leads to the same CRC32 checksum
+#crc32(aes_key_generation_source) = 459881589 but
+#crc32(TopSecretsEtM) = 459881589 too => No keys where shared!
+#Use https://github.com/bediger4000/crc32-file-collision-generator
+#to generate your own CRC32 collisions if you don't believe my proof.
+crc32_checksum = {
+	'aes_kek_generation_source': 2545229389,
+	'aes_key_generation_source': 459881589,
+	'titlekek_source': 3510501772,
+	'key_area_key_application_source': 4130296074,
+	'key_area_key_ocean_source': 3975316347,
+	'key_area_key_system_source': 4024798875,
+	'master_key_00': 3540309694,
+	'master_key_01': 3477638116,
+	'master_key_02': 2087460235,
+	'master_key_03': 4095912905,
+	'master_key_04': 3833085536,
+	'master_key_05': 2078263136,
+	'master_key_06': 2812171174,
+	'master_key_07': 1146095808,
+	'master_key_08': 1605958034,
+	'master_key_09': 3456782962
+}
 
 def getMasterKeyIndex(i):
 	if i > 0:
@@ -56,72 +84,82 @@ def generateKek(src, masterKey, kek_seed, key_seed):
 		return src_kek
 
 def unwrapAesWrappedTitlekey(wrappedKey, keyGeneration):
-	aes_kek_generation_source = uhx(keys['aes_kek_generation_source'])
-	aes_key_generation_source = uhx(keys['aes_key_generation_source'])
+	aes_kek_generation_source = getKey('aes_kek_generation_source')
+	aes_key_generation_source = getKey('aes_key_generation_source')
 
-	kek = generateKek(uhx(keys['key_area_key_application_source']), uhx(keys['master_key_0' + str(keyGeneration)]), aes_kek_generation_source, aes_key_generation_source)
+	kek = generateKek(getKey('key_area_key_application_source'), getMasterKey(keyGeneration), aes_kek_generation_source, aes_key_generation_source)
 
 	crypto = aes128.AESECB(kek)
 	return crypto.decrypt(wrappedKey)
 
 def getKey(key):
 	if key not in keys:
-		raise IOError('%s missing from keys.txt' % key)
-	return uhx(keys[key])
+		raise IOError('{0} missing from {1}'.format(key, loadedKeysPath))
+	foundKey = uhx(keys[key])
+	foundKeyChecksum = crc32(foundKey)
+	if key in crc32_checksum:
+		if crc32_checksum[key] != foundKeyChecksum:
+			raise IOError('{0} from {1} is invalid (crc32 missmatch)'.format(key, loadedKeysPath))
+	elif current_process().name == 'MainProcess':
+		Print.info('Unconfirmed: crc32({0}) = {1}'.format(key, foundKeyChecksum))
+	return foundKey
 
-def masterKey(masterKeyIndex):
-	return getKey('master_key_0' + str(masterKeyIndex))
+def getMasterKey(masterKeyIndex):
+	return getKey('master_key_{0:02}'.format(masterKeyIndex))
+	
+def existsMasterKey(masterKeyIndex):
+	return 'master_key_{0:02}'.format(masterKeyIndex) in keys
 
 def load(fileName):
 	try:
 		global keyAreaKeys
 		global titleKeks
-	
+		global loadedKeysPath
+		loadedKeysPath = fileName
+		
 		with open(fileName, encoding="utf8") as f:
 			for line in f.readlines():
 				r = re.match('\s*([a-z0-9_]+)\s*=\s*([A-F0-9]+)\s*', line, re.I)
 				if r:
 					keys[r.group(1)] = r.group(2)
 		
-	
-		aes_kek_generation_source = uhx(keys['aes_kek_generation_source'])
-		aes_key_generation_source = uhx(keys['aes_key_generation_source'])
-	
-		keyAreaKeys = []
-		for i in range(10):
-			keyAreaKeys.append([None, None, None])
-	
+		aes_kek_generation_source = getKey('aes_kek_generation_source')
+		aes_key_generation_source = getKey('aes_key_generation_source')
+		titlekek_source = getKey('titlekek_source')
+		key_area_key_application_source = getKey('key_area_key_application_source')
+		key_area_key_ocean_source = getKey('key_area_key_ocean_source')
+		key_area_key_system_source = getKey('key_area_key_system_source')
 		
-		for i in range(10):
-			masterKeyName = 'master_key_0' + str(i)
-			if masterKeyName in keys.keys():
-	
-				masterKey = uhx(keys[masterKeyName])
-				crypto = aes128.AESECB(masterKey)
-				titleKeks.append(crypto.decrypt(uhx(keys['titlekek_source'])).hex())
-				keyAreaKeys[i][0] = generateKek(uhx(keys['key_area_key_application_source']), masterKey, aes_kek_generation_source, aes_key_generation_source)
-				keyAreaKeys[i][1] = generateKek(uhx(keys['key_area_key_ocean_source']), masterKey, aes_kek_generation_source, aes_key_generation_source)
-				keyAreaKeys[i][2] = generateKek(uhx(keys['key_area_key_system_source']), masterKey, aes_kek_generation_source, aes_key_generation_source)
-			else:
-				titleKeks.append('0' * 32)
-				raise IOError('{0} missing from keys.txt'.format(masterKeyName))
+		keyAreaKeys = []
+		for i in range(32):
+			keyAreaKeys.append([None, None, None])
+		
+		for i in range(32):
+			if not existsMasterKey(i):
+				continue
+			masterKey = getMasterKey(i)
+			crypto = aes128.AESECB(masterKey)
+			titleKeks.append(crypto.decrypt(titlekek_source).hex())
+			keyAreaKeys[i][0] = generateKek(key_area_key_application_source, masterKey, aes_kek_generation_source, aes_key_generation_source)
+			keyAreaKeys[i][1] = generateKek(key_area_key_ocean_source, masterKey, aes_kek_generation_source, aes_key_generation_source)
+			keyAreaKeys[i][2] = generateKek(key_area_key_system_source, masterKey, aes_kek_generation_source, aes_key_generation_source)
 	except BaseException as e:
 		Print.error(format_exc())
 		Print.error(str(e))
 
 
 
-keyScriptPath = os.path.dirname(os.path.abspath(sys.argv[0]))
-#While loop to get rid of things like C:\Python37\Scripts\nsz.exe\__main__.py
-while not os.path.isdir(keyScriptPath):
-	keyScriptPath = os.path.dirname(keyScriptPath)
-keypath = os.path.join(keyScriptPath, 'keys.txt')
-dumpedKeys = os.path.join(Path.home(), ".switch", "prod.keys")
-if os.path.isfile(keypath):
-	load(keypath)
-elif os.path.isfile(dumpedKeys):
-	load(dumpedKeys)
+keyScriptPath = Path(sys.argv[0])
+#While loop to get rid of things like C:\\Python37\\Scripts\\nsz.exe\\__main__.py
+while not keyScriptPath.is_dir():
+	keyScriptPath = keyScriptPath.parents[0]
+keypath = keyScriptPath.joinpath('keys.txt')
+dumpedKeys = Path.home().joinpath(".switch", "prod.keys")
+if keypath.is_file():
+	load(str(keypath))
+elif dumpedKeys.is_file():
+	load(str(dumpedKeys))
 else:
-	errorMsg = "{0} or {1} not found!\nPlease dump your keys using https://github.com/shchmue/Lockpick_RCM/releases".format(keypath, dumpedKeys)
+	errorMsg = "{0} or {1} not found!\nPlease dump your keys using https://github.com/shchmue/Lockpick_RCM/releases".format(str(keypath), str(dumpedKeys))
 	raise FileNotFoundError(errorMsg)
 
