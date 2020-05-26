e8e024d2cc2e0cbf9a0836b490902fc7b5158fdf
nicoboss <nico@bosshome.ch>
2020-05-26 20:26:08 +0200

    Starting nsz.py from within a different working directory finally works what fixes the current Azure Pipeline issue

--- nsz.py
+++ nsz.py
@@ -4,10 +4,15 @@
 # This is needed as multiprocessing shouldn't include nsz
 # as it won't be able to optain __main__.__file__ and so crash inside Keys.py
 if __name__ == '__main__':
-	import sys
+	import sys, pathlib
 	if sys.hexversion < 0x03060000:
 		raise ImportError("NSZ requires at least Python 3.6!\nCurrent python version is " + sys.version)
 	import multiprocessing
 	multiprocessing.freeze_support()
-	import nsz
+	try:
+		import nsp
+	except ImportError:
+		path = pathlib.Path(__file__).resolve().parent.absolute()
+		sys.path.append(str(path))
+		import nsz
 	nsz.main()
