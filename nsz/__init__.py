nicoboss/nsz

commit 791af79192a3e8f978168edd44c8d0317f39395b

Added titleid and version to the file list and highly improved its design

nsz/gui/GameList.py
@@ -17,7 +17,7 @@ from functools import partial
 from nsz.gui.DraggableScrollbar import *
 from nsz.gui.GuiPath import *
 from nsz.PathTools import *
-
+from nsz import FileExistingChecks
 
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
 	
@@ -46,12 +46,14 @@ class SelectableLabel(RecycleDataViewBehavior, GridLayout):
 	index = None
 	selected = BooleanProperty(False)
 	selectable = BooleanProperty(True)
-	cols = 3
+	cols = 4
 
 	def refresh_view_attrs(self, rv, index, data):
 		self.index = index
 		self.filename_text = data['0']
-		self.filesize_text = data['1']
+		self.titleid = data['1']
+		self.version = data['2']
+		self.filesize_text = data['3']
 		return super(SelectableLabel, self).refresh_view_attrs(
 			rv, index, data)
 
@@ -81,7 +83,7 @@ class RV(RecycleView):
 	def refresh(self, items):
 		self.data.clear()
 		for path in items:
-			self.data.append({'0': path, '1': self.sizeof_fmt(items[path])})
+			self.data.append({'0': path, '1': items[path][0], '2': 'v' + str(items[path][1]), '3': self.sizeof_fmt(items[path][2])})
 		self.refresh_from_data()
 			
 	def sizeof_fmt(self, num, suffix='B'):
@@ -119,7 +121,8 @@ class GameList(StackLayout):
 			self.refresh()
 		elif path.is_file():
 			if isGame(path) or isCompressedGameFile(path):
-				self.filelist[fullPath] = path.stat().st_size
+				(titleIDExtracted, versionExtracted) = FileExistingChecks.ExtractTitleIDAndVersion(fullPath, True)
+				self.filelist[fullPath] = (titleIDExtracted, versionExtracted, path.stat().st_size)
 				self.refresh()
 		else:
 			print("Warning: {0} isn't a file or folder!".format(fullPath))

nsz/gui/DraggableScrollbar.py
@@ -23,7 +23,7 @@ class DraggableScrollbar(GridLayout):
 		self.orientation='lr-bt'
 
 		#the last child of layout1 and this will act as the draggable scrollbar
-		self.slider = Slider(min=0, max=1, value=25, orientation='vertical', step=0.01, size_hint=(0.1, 0.95))
+		self.slider = Slider(min=0, max=1, value=25, orientation='vertical', step=0.01, size_hint=(0.03, 0.95))
 
 		recycleView.bind(scroll_y=partial(self.slider_change))
 

nsz/gui/layout/GameList.kv
@@ -8,29 +8,84 @@
 			size: self.size
 	cover_source: ''
 	filename_text: ''
+	titleid: ''
+	version: ''
 	filesize_text: '0 KB'
 	pos: self.pos
 	size: self.size
-	padding: 20
 	Label:
 		id: id_filename
+		valign: "top"
 		halign: "left"
-		text_size: self.size
+		pos: 0, 0
+		size_hint: None, None
+		text_size: self.width, root.height
 		text: root.filename_text
+		size: root.width-300, self.texture_size[1]
+		on_size: root.height = self.height
+		padding: [3, 2]
+		canvas.before:
+			Color:
+				rgba: (1.0, 0.0, 0.0, 0.1)
+			Rectangle:
+				pos: self.pos
+				size: self.size
+	Label:
+		id: id_titleid
+		valign: "top"
+		halign: "right"
+		pos: root.width-300, 0
+		size_hint: None, None
+		text_size: self.width, root.height
+		text: root.titleid
+		size: 140, self.texture_size[1]
+		padding: [3, 2]
+		canvas.before:
+			Color:
+				rgba: (1.0, 1.0, 0.0, 0.1)
+			Rectangle:
+				pos: self.pos
+				size: self.size
+	Label:
+		id: id_version
+		valign: "top"
+		halign: "right"
+		pos: root.width-160, 0
+		size_hint: None, None
+		text_size: self.width, root.height
+		text: root.version
+		size: 80, self.texture_size[1]
+		padding: [3, 2]
+		canvas.before:
+			Color:
+				rgba: (0.0, 1.0, 1.0, 0.1)
+			Rectangle:
+				pos: self.pos
+				size: self.size
 	Label:
 		id: id_size
+		valign: "top"
 		halign: "right"
-		text_size: self.size
+		pos: root.width-80, 0
+		size_hint: None, None
+		text_size: self.width, root.height
 		text: root.filesize_text
-
+		size: 80, self.texture_size[1]
+		padding: [3, 2]
+		canvas.before:
+			Color:
+				rgba: (0.0, 0.0, 1.0, 0.1)
+			Rectangle:
+				pos: self.pos
+				size: self.size
 
 <RV>:
 	scroll_y: 0.5
 	viewclass: 'SelectableLabel'
 	SelectableRecycleBoxLayout:
 		id: selectableRecycleBoxLayout
-		default_size: None, dp(30)
-		default_size_hint: 0.95, None
+		default_size: None, dp(40)
+		default_size_hint: 0.99, None
 		size_hint_y: None
 		height: self.minimum_height
 		orientation: 'vertical'

commit 4f83aa0898f9bb62a3157b8c8c17a85c5c9c158b

Fixed NCZ to NCA decompression got broken by 791af79 and 5196612

nsz/__init__.py
@@ -228,7 +228,7 @@ def main():
 								continue
 						elif filePath.suffix == '.ncz':
 							outFile = Path(changeExtension(outFolder.joinpath(filePath.name), ".nca"))
-							if not args.overwrite and outfile.is_file():
+							if not args.overwrite and outFile.is_file():
 								Print.info('{0} with the same file name already exists in the output directory.\n'\
 								'If you want to overwrite it use the -w parameter!'.format(outFile.name))
 								continue

nsz/gui/GameList.py
@@ -121,7 +121,10 @@ class GameList(StackLayout):
 			self.refresh()
 		elif path.is_file():
 			if isGame(path) or isCompressedGameFile(path):
-				(titleIDExtracted, versionExtracted) = FileExistingChecks.ExtractTitleIDAndVersion(fullPath, True)
+				if isGame(path):
+					(titleIDExtracted, versionExtracted) = FileExistingChecks.ExtractTitleIDAndVersion(fullPath, True)
+				else:
+					(titleIDExtracted, versionExtracted) = ("None", 0)
 				self.filelist[fullPath] = (titleIDExtracted, versionExtracted, path.stat().st_size)
 				self.refresh()
 		else: