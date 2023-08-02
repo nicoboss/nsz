ecc4c57b4fda584f6e90d06606d6f9d224c84f53
nicoboss <nico@bosshome.ch>
2023-08-02 22:23:58 +0200

    Removed locks from ThreadSafeCounter.py which makes the command line version of NSZ fully Android compatible but introduces race conditions. The issue with locks was: "ImportError: This platform lacks a functioning sem_open implementation, therefore, the required synchronization primitives needed will not function, see issue 3770."

--- nsz/ThreadSafeCounter.py
+++ nsz/ThreadSafeCounter.py
@@ -1,18 +1,13 @@
-from multiprocessing import Manager, Lock
+from multiprocessing import Manager
 
 class Counter(object):
 	def __init__(self, manager, initval=0):
 		self.val = manager.Value('i', initval)
-		self.lock = Lock()
 	def set(self, newValue):
-		with self.lock:
-			self.val.value = newValue
+		self.val.value = newValue
 	def increment(self):
-		with self.lock:
-			self.val.value += 1
+		self.val.value += 1
 	def decrement(self):
-		with self.lock:
-			self.val.value -= 1
+		self.val.value -= 1
 	def value(self):
-		with self.lock:
-			return self.val.value
+		return self.val.value
